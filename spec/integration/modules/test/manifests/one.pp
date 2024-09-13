2ebb26e29db9fe33b8c6f05821df0f95e87d3b6e
jhongturney <jhongturney@github.com>
2024-09-13 15:33:50 -0700

    update for newer fact structure

--- spec/integration/modules/test/manifests/one.pp
+++ spec/integration/modules/test/manifests/one.pp
@@ -1,12 +1,12 @@
 class test::one {
-  file { "/tmp/system-info.txt":
+  file { '/tmp/system-info.txt':
     ensure  => file,
-    owner   => $::id,
-    group   => $::gid,
-    content => template("test/one/system-info.txt"),
+    owner   => $facts['identity']['user'],
+    group   => $facts['identity']['group'],
+    content => template('test/one/system-info.txt'),
   }
 
-  file { "/etc/hosts":
-    content => "127.0.0.1 localhost ${::shorthost}",
+  file { '/etc/hosts':
+    content => "127.0.0.1 localhost ${facts['networking']['hostname']}",
   }
 }
